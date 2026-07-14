172f3213c36d3643b82bfe141ff55b784d0274cd
zakkdruzer <zakkdruzer@gmail.com>
2026-07-13 23:20:28 -0300

    feat: estructura base completada

---
+++ ejercicio-1/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Ej1Semaforo from './components/Ej1Semaforo.vue';
+import Ej2Saludo from './components/Ej2Saludo.vue';
+import Ej3Modificadores from './components/Ej3Modificadores.vue';
+import Ej4Textarea from './components/Ej4Textarea.vue';
+import Ej5Checkbox from './components/Ej5Checkbox.vue';
+import Ej6Radio from './components/Ej6Radio.vue';
+import Ej7Select from './components/Ej7Select.vue';
+import EjFinalReserva from './components/EjFinalReserva.vue';
+</script>
+
+<template>
+  <Ej1Semaforo />
+  <Ej2Saludo />
+  <Ej3Modificadores />
+  <Ej4Textarea />
+  <Ej5Checkbox />
+  <Ej6Radio />
+  <Ej7Select />
+  <EjFinalReserva />
+</template>
