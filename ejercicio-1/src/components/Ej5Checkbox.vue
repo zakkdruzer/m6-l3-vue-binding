a8c1f27fcd4d270186cdb52f8110a4f62bd9d801
zakkdruzer <zakkdruzer@gmail.com>
2026-07-13 23:56:52 -0300

    feat: ejercicio 5 completado

--- ejercicio-1/src/components/Ej5Checkbox.vue
+++ ejercicio-1/src/components/Ej5Checkbox.vue
@@ -1,11 +1,68 @@
 <script setup>
-  import { ref } from 'vue'
+import { ref } from 'vue'
 
-  // TODO: declara tus variables reactivas con ref()
-  // Con <script setup> NO hay return: todo lo que declaras acá
-  // queda disponible en el template automáticamente.
+const suscrito = ref(false)
+
+const hobbies = ref([])
+
+const modo = ref('gratis')
 </script>
 
 <template>
-  <!-- TODO: aquí va tu HTML con las directivas -->
+  <div class="ej-checkbox">
+    <section>
+      <h3>Suscripción (booleano)</h3>
+      <label>
+        <input type="checkbox" v-model="suscrito" />
+        Suscribirme al newsletter
+      </label>
+      <p>Suscrito: {{ suscrito }}</p>
+    </section>
+    <section>
+      <h3>Hobbies (array)</h3>
+
+      <label>
+        <input
+          type="checkbox"
+          value="leer"
+          v-model="hobbies"
+        />
+        Leer
+      </label>
+
+      <label>
+        <input
+          type="checkbox"
+          value="deporte"
+          v-model="hobbies"
+        />
+        Deporte
+      </label>
+
+      <label>
+        <input
+          type="checkbox"
+          value="musica"
+          v-model="hobbies"
+        />
+        Música
+      </label>
+
+      <p>Hobbies seleccionados: {{ hobbies }}</p>
+      <p>Cantidad: {{ hobbies.length }}</p>
+    </section>
+    <section>
+      <h3>Modo con valores personalizados</h3>
+      <label>
+        <input
+          type="checkbox"
+          v-model="modo"
+          :true-value="'pro'"
+          :false-value="'gratis'"
+        />
+        Modo PRO
+      </label>
+      <p>Modo actual: {{ modo }}</p>
+    </section>
+  </div>
 </template>
