55675dcd8d1889c05126a7327877811aa3a4e38d
zakkdruzer <zakkdruzer@gmail.com>
2026-07-14 09:51:21 -0300

    feat: ejercicio 6 completado

--- ejercicio-1/src/components/Ej6Radio.vue
+++ ejercicio-1/src/components/Ej6Radio.vue
@@ -1,11 +1,59 @@
 <script setup>
-  import { ref } from 'vue'
+import { ref } from 'vue'
 
-  // TODO: declara tus variables reactivas con ref()
-  // Con <script setup> NO hay return: todo lo que declaras acá
-  // queda disponible en el template automáticamente.
+const calificacion = ref('')
 </script>
 
 <template>
-  <!-- TODO: aquí va tu HTML con las directivas -->
+  <div class="ej-radio">
+    <h3>¿Cómo calificarías la clase?</h3>
+
+    <label>
+      <input
+        type="radio"
+        value="Excelente"
+        v-model="calificacion"
+      />
+      Excelente
+    </label>
+
+    <label>
+      <input
+        type="radio"
+        value="Buena"
+        v-model="calificacion"
+      />
+      Buena
+    </label>
+
+    <label>
+      <input
+        type="radio"
+        value="Regular"
+        v-model="calificacion"
+      />
+      Regular
+    </label>
+
+    <label>
+      <input
+        type="radio"
+        value="Mala"
+        v-model="calificacion"
+      />
+      Mala
+    </label>
+
+    <p>Opción elegida: {{ calificacion || 'ninguna' }}</p>
+
+    <p v-if="calificacion === 'Excelente' || calificacion === 'Buena'">
+      ¡Gracias por tu buena evaluación!
+    </p>
+    <p v-else-if="calificacion === 'Regular'">
+      Gracias por tu sinceridad, veremos cómo mejorar.
+    </p>
+    <p v-else-if="calificacion === 'Mala'">
+      Sentimos que tu experiencia no fue buena, cuéntanos más.
+    </p>
+  </div>
 </template>
